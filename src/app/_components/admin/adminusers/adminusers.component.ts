f601832c211f1c9f222ac6ec5abc553d0ecc53cb
bazookajoegolf <bazookajoegolf@outlook.com>
2023-02-01 10:43:50 -0700

    angular 15

--- src/app/_components/admin/adminusers/adminusers.component.ts
+++ src/app/_components/admin/adminusers/adminusers.component.ts
@@ -9,15 +9,9 @@ import { MatSort } from '@angular/material/sort';
   styleUrls: ['./adminusers.component.css']
 })
 export class AdminusersComponent implements OnInit {
-<<<<<<< HEAD
   @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: false}) sort: MatSort;
   //dataSource: AdminusersDataSource;
-=======
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
-  dataSource: AdminusersDataSource;
->>>>>>> 6ba2f63a132e34b716946e103bfcb6dc1308fa15
 
     /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['id', 'name'];
