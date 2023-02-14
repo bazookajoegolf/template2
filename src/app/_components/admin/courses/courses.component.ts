3af2eaed170b8cb12404f89431d8d0c9b180168c
bazookajoegolf <bazookajoegolf@outlook.com>
2023-02-14 13:32:30 -0700

    15.3

--- src/app/_components/admin/courses/courses.component.ts
+++ src/app/_components/admin/courses/courses.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input, OnChanges, SimpleChanges, Output, EventEmitter} from '@angular/core';
 
 import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -24,15 +24,21 @@ interface Course {
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css','./../../../assets/css/bkgd.css']
+  styleUrls: ['./courses.component.css']
 })
-export class CoursesComponent implements OnInit {
+export class CoursesComponent implements OnInit, OnChanges {
   panelOpenState = false;
   name;
   id;
   form: UntypedFormGroup;
   course: Course[];
   editMode:boolean = false;
+
+@Input()
+  selectedCourse: Course;
+
+// @Output()
+//   getUpdate = new EventEmitter();  
 
   @ViewChild('dd') dd: ElementRef;
 
@@ -51,19 +57,35 @@ export class CoursesComponent implements OnInit {
     });
     this.form.disable();
 
-    this.getCourses();
+    //this.selectCourse(this.selectedCourse);
+    
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+    if(this.selectedCourse) {
+      this.selectCourse(this.selectedCourse);
+      this.name = this.selectedCourse.name;
+    }  
 
   }
+
+
+
   get f() {
     return this.form.controls
     };
 
-  selectCourse(event: Event) {
-    // this.name = this.course.name;
-    this.name = (event.target as HTMLSelectElement).value;
 
-    const x = this.course.find(o => o.name === this.name);
-    console.log(x);
+  // selectedCourse($event: any){
+  //   console.log("receiving event from Output of parent");
+  // }  
+
+  selectCourse(y:Course) {
+    // this.name = this.course.name;
+    //this.name = (event.target as HTMLSelectElement).value;
+    const x = y;
+
     this.form.patchValue({ 'name': x.name });
     this.form.patchValue({ 'address': x.address });
     this.form.patchValue({ 'city': x.city });
@@ -73,18 +95,18 @@ export class CoursesComponent implements OnInit {
     this.id = x._id;
   }
 
-  getCourses() {
+  // getCourses() {
 
-    this.courses.getCourses()
-      .subscribe((courses) => {
-        if (courses) {
-          this.course = courses;
-        }
-      }, (error) => {
-        this.alert.error(error.error.message);
-      });
+  //   this.courses.getCourses()
+  //     .subscribe((courses) => {
+  //       if (courses) {
+  //         this.course = courses;
+  //       }
+  //     }, (error) => {
+  //       this.alert.error(error.error.message);
+  //     });
 
-  }
+  // }
 
   onNew() {
     this.form.enable();
@@ -109,7 +131,8 @@ export class CoursesComponent implements OnInit {
       this.alert.success("Course deleted");
       this.form.disable();
       this.editMode=false;
-      this.getCourses();
+    //  this.getUpdate.emit("Deleted!");
+     // this.getCourses();
     },
       (error) => {
         this.alert.error(error.error.message);
@@ -145,7 +168,7 @@ export class CoursesComponent implements OnInit {
         this.alert.success("Settings Saved!");
         this.form.disable();
         this.editMode=false;
-        this.getCourses();
+     //   //this.getCourses();
       },
         (error) => {
           this.alert.error(error.error.message);
@@ -160,7 +183,8 @@ export class CoursesComponent implements OnInit {
         this.alert.success("Settings Saved!");
         this.form.disable();
         this.editMode=false;
-        this.getCourses();
+    //    this.getUpdate.emit("Saved!");
+        //this.getCourses();
 
       },
         (error) => {
